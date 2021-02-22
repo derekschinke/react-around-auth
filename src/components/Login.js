721c3b1e32bd8579700904453ac8de0487e463e5
Derek Schinke <derekschinke@me.com>
2021-02-22 15:29:58 -0500

    feature: some mobile stuff

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Login() {
   return (
-    <section>
+    <section className="login">
       <h2 className="login__title">Log in</h2>
       <Link to="/signup" className="login__link button">
         Not a member yet? Sign up here!
--- src/components/Register.js
+++ src/components/Register.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Register() {
   return (
-    <section>
+    <section className="login">
       <h2 className="login__title">Sign up</h2>
       <Link to="/signin" className="login__link button">
         Already a member? Log in here!
