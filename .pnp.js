38cf204b0fe26fc5e4b546c2c12a5c237e3a2d62
Derek Schinke <derekschinke@me.com>
2021-02-28 23:30:31 -0500

    upgrade: yarn upgrade

--- .pnp.js
+++ .pnp.js
@@ -60,7 +60,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["eslint-plugin-testing-library", "virtual:99d74bc85544bb2995c3cab9bb2eb8817f0ecae4bc99965b7baab0747a9c9c3912ac1df68affd52afaf256499a73e51eed05ed38cbf985c40686d3760d905c36#npm:3.10.1"],
             ["gh-pages", "npm:3.1.0"],
             ["hamburger-react", "virtual:99d74bc85544bb2995c3cab9bb2eb8817f0ecae4bc99965b7baab0747a9c9c3912ac1df68affd52afaf256499a73e51eed05ed38cbf985c40686d3760d905c36#npm:2.4.0"],
-            ["husky", "npm:5.1.1"],
+            ["husky", "npm:5.1.2"],
             ["lint-staged", "npm:10.5.4"],
             ["normalize.css", "npm:8.0.1"],
             ["prettier", "npm:2.2.1"],
@@ -13277,10 +13277,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["husky", [
-        ["npm:5.1.1", {
-          "packageLocation": "./.yarn/cache/husky-npm-5.1.1-2cc506f396-130f2298be.zip/node_modules/husky/",
+        ["npm:5.1.2", {
+          "packageLocation": "./.yarn/cache/husky-npm-5.1.2-0900fa47d5-2c20d7132f.zip/node_modules/husky/",
           "packageDependencies": [
-            ["husky", "npm:5.1.1"]
+            ["husky", "npm:5.1.2"]
           ],
           "linkType": "HARD",
         }]
@@ -18394,7 +18394,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["eslint-plugin-testing-library", "virtual:99d74bc85544bb2995c3cab9bb2eb8817f0ecae4bc99965b7baab0747a9c9c3912ac1df68affd52afaf256499a73e51eed05ed38cbf985c40686d3760d905c36#npm:3.10.1"],
             ["gh-pages", "npm:3.1.0"],
             ["hamburger-react", "virtual:99d74bc85544bb2995c3cab9bb2eb8817f0ecae4bc99965b7baab0747a9c9c3912ac1df68affd52afaf256499a73e51eed05ed38cbf985c40686d3760d905c36#npm:2.4.0"],
-            ["husky", "npm:5.1.1"],
+            ["husky", "npm:5.1.2"],
             ["lint-staged", "npm:10.5.4"],
             ["normalize.css", "npm:8.0.1"],
             ["prettier", "npm:2.2.1"],
