2dfa78f681900e4a498f00291ee200c9e78aa6ed
Derek Schinke <derekschinke@me.com>
2021-03-08 15:10:04 -0500

    feature: form template

--- src/components/Register.js
+++ src/components/Register.js
@@ -4,6 +4,15 @@ function Register() {
   return (
     <section className="login">
       <h2 className="login__title">Sign up</h2>
+      <form>
+        <label>
+          <input></input>
+        </label>
+        <label>
+          <input></input>
+        </label>
+        <input></input>
+      </form>
       <Link to="/signin" className="login__link button">
         Already a member? Log in here!
       </Link>
