e13ebc4d0fc93ac9af00c5408b52d8995e271d57
Derek Schinke <derekschinke@me.com>
2021-03-21 19:47:37 -0400

    feature: login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,29 @@
 import { Link } from 'react-router-dom';
+import PopupWithForm from './PopupWithForm';
 
 function Login() {
   return (
     <section className="account">
       <h2 className="account__title">Log in</h2>
+      <PopupWithForm
+        name="account"
+        buttonValue="Log in"
+        submitButtonClass="button_type_submit_type_account"
+      >
+        <label>
+          <input
+            className="popup__input popup__input_type_account"
+            placeholder="Email"
+          ></input>
+        </label>
+        <label>
+          <input
+            className="popup__input popup__input_type_account"
+            placeholder="Password"
+          ></input>
+        </label>
+        <div className="popup__spacer"></div>
+      </PopupWithForm>
       <Link to="/signup" className="account__link button">
         Not a member yet? Sign up here!
       </Link>
