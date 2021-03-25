8443c56a97dea9e0250fe83831f68628db28c629
Derek Schinke <derekschinke@me.com>
2021-03-24 22:47:20 -0400

    feature: auth arrow functions

---
+++ src/utils/auth.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-console */
+export const BASE_URL = 'https://register.nomoreparties.co';
+
+export const register = (email, password) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((response) => {
+      try {
+        if (response.status === 200) {
+          return response.json();
+        }
+        return null;
+      } catch (e) {
+        return e;
+      }
+    })
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => console.log(err));
+};
+
+export const authorize = (email, password) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.user) {
+        localStorage.setItem('jwt', data.jwt);
+        return data;
+      }
+      return null;
+    })
+    .catch((err) => console.log(err));
+};
+
+export const getContent = (token) => {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => data);
+};
