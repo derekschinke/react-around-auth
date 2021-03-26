1ea691fc3d8899e570e4bd77d2da7031631c96c1
Derek Schinke <derekschinke@me.com>
2021-03-25 22:36:20 -0400

    feature: yeah idk if this works at all i just need to upgrade yarn lol

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,27 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import PopupWithForm from './PopupWithForm';
 
 function Register() {
+  const history = useHistory();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.PreventDefault();
+    if (localStorage.getItem('jwt')) {
+      history.push('/');
+    }
+  }
+
   return (
     <section className="account">
       <h2 className="account__title">Sign up</h2>
@@ -9,12 +29,15 @@ function Register() {
         name="account"
         buttonValue="Sign up"
         submitButtonClass="button_type_submit_type_account"
+        onSubmit={handleSubmit}
       >
         <label>
           <input
             className="popup__input popup__input_type_account"
             placeholder="Email"
             required
+            value={email}
+            onChange={handleEmailChange}
           ></input>
         </label>
         <label>
@@ -22,6 +45,8 @@ function Register() {
             className="popup__input popup__input_type_account"
             placeholder="Password"
             required
+            value={password}
+            onChange={handlePasswordChange}
           ></input>
         </label>
         <div className="popup__spacer"></div>
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-export const BASE_URL = 'https://register.nomoreparties.co';
+const BASE_URL = 'https://register.nomoreparties.co';
 
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
@@ -10,18 +10,8 @@ export const register = (email, password) => {
     },
     body: JSON.stringify({ email, password }),
   })
-    .then((response) => {
-      try {
-        if (response.status === 200) {
-          return response.json();
-        }
-        return null;
-      } catch (e) {
-        return e;
-      }
-    })
     .then((res) => {
-      return res;
+      return res.json();
     })
     .catch((err) => console.log(err));
 };
@@ -35,18 +25,20 @@ export const authorize = (email, password) => {
     },
     body: JSON.stringify({ email, password }),
   })
-    .then((response) => response.json())
+    .then((res) => res.json())
     .then((data) => {
       if (data.user) {
         localStorage.setItem('jwt', data.jwt);
         return data;
       }
-      return null;
+      return new Error('Something went wrong');
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
-export const getContent = (token) => {
+export const checkToken = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
     method: 'GET',
     headers: {
@@ -56,5 +48,8 @@ export const getContent = (token) => {
     },
   })
     .then((res) => res.json())
-    .then((data) => data);
+    .then((data) => data)
+    .catch((err) => {
+      console.log(err);
+    });
 };
