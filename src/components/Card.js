75d87415b8bdd105a8888ff2cbcaa37500a2e0f7
Derek Schinke <derekschinke@me.com>
2021-04-21 22:33:06 -0400

    fix: delete button

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import CurrentUserContext from '../contexts/CurrentUserContext';
 function Card(props) {
   const currentUser = useContext(CurrentUserContext);
 
-  const isOwn = props.card.owner._id === currentUser._id;
+  const isOwn = props.card.owner === currentUser._id;
   const isLiked = props.card.likes.some((i) => i._id === currentUser._id);
 
   return (
