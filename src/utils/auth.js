bdf9da5154b8859eb266b22e8471e87fd63f8dea
Derek Schinke <derekschinke@me.com>
2021-04-27 00:20:05 -0400

    refactor: from Promise chaining to async/await chaining

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,70 +3,65 @@ import config from './constants';
 
 const BASE_URL = config.url.API_URL;
 
-export const register = (email, password) => {
-  return fetch(`${BASE_URL}/signup`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email, password }),
-  })
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      throw new Error('Registration failed');
-    })
-    .catch((err) => {
-      console.log(err);
+export const register = async (email, password) => {
+  try {
+    const res = await fetch(`${BASE_URL}/signup`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
     });
+    if (res.ok) {
+      return res.json();
+    }
+    throw new Error('Registration failed');
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-export const authorize = (email, password) => {
-  return fetch(`${BASE_URL}/signin`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email, password }),
-  })
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      throw new Error('Authorization failed');
-    })
-    .then((data) => {
+export const authorize = async (email, password) => {
+  try {
+    const res = await fetch(`${BASE_URL}/signin`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    if (res.ok) {
+      const data = await res.json();
       if (data.token) {
         localStorage.setItem('token', data.token);
         return data;
       }
       throw new Error('No token received');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    }
+    throw new Error('Authorization failed');
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-export const checkToken = (token) => {
-  return fetch(`${BASE_URL}/users/me`, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-  })
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      throw new Error('Authorization failed');
-    })
-    .then((data) => data)
-    .catch((err) => {
-      console.log(err);
+export const checkToken = async (token) => {
+  try {
+    const res = await fetch(`${BASE_URL}/users/me`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
     });
+    if (res.ok) {
+      const data = await res.json();
+      return data;
+    }
+    throw new Error('Authorization failed');
+  } catch (err) {
+    console.log(err);
+  }
 };
